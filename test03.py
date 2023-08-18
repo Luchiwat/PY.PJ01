ff6d0109467e2ed148fb69a0f0f39c6a78dfbaab
Chavalvit <s6519410032@sau.ac.th>
2023-08-18 11:23:31 +0700

    code v.1:Ls 18/8/2566

---
+++ test03.py
@@ -0,0 +1,10 @@
+print('aaa'); print('bbb')
+print('ccc')
+print('111111111\n00000000\t222')
+print('adada"rerer"')
+print("adada'oj,kfc'")
+print('''    sadad
+    dasdasd
+    adsad''')
+A=443
+print(A)
